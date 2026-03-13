3951d880472875dbb4cd38697cfd85ea28fc4223
Dr. Jovo <jonathanckleven@gmail.com>
2026-03-12 18:08:14 -0700

    GUI Organization and Functionality
    - Organized the GUI into a grid of frames for side-by-side elements.
    - Added the ability to change the port by which the server is accessible.
    - Added requirements.txt to store all the Python dependencies required to run the project on another device while in development.

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import sys
 
 server_process = None
 
+
 # --- Functions --- #
 # Start server in a subprocess to keep the GUI from freezing
 def start_server():
     global server_process
 
+    print("Booting Server...")
     if server_process == None:
         server_process = subprocess.Popen(
             [
@@ -20,7 +22,7 @@ def start_server():
                 "--host",
                 "0.0.0.0",
                 "--port",
-                "7000"
+                port_entry.get()
             ]
         )
     server_status.configure(text="Server Status: up")
@@ -28,12 +30,17 @@ def start_server():
 # Shut down the server, but keep the GUI up
 def stop_server():
     global server_process
-
+    
+    print("Stopping Server...")
     if server_process:
         server_process.terminate()
         server_process.wait()
         server_process = None
     server_status.configure(text="Server Status: down")
+
+
+# --- Server Default Settings --- #
+default_port = 7000
 
 # --- Window Setup --- #
 # System Settings
@@ -42,19 +49,34 @@ customtkinter.set_default_color_theme("blue")
 
 # App Frame
 app = customtkinter.CTk()
-app.geometry("960x720")
 app.title("Server Manager")
+app.resizable(False, False)
 
 # --- UI Elements --- #
-# Start / Stop
-server_start_button = customtkinter.CTkButton(app, text="Start Server", command=start_server)
-server_stop_button = customtkinter.CTkButton(app, text="Stop Server", command=stop_server)
-server_start_button.pack(padx=10, pady=10)
-server_stop_button.pack(padx=10, pady=10)
+# Frames
+title_frame = tkinter.Frame(app, bg="#242424")
+port_frame = tkinter.Frame(app, bg="#242424")
+server_toggle_frame = tkinter.Frame(app, bg="#242424")
+title_frame.pack(side=tkinter.TOP)
+port_frame.pack(side=tkinter.TOP)
+server_toggle_frame.pack(side=tkinter.BOTTOM)
 
 # Server Status
 server_status = customtkinter.CTkLabel(app, text="Server Status: down")
-server_status.pack(padx=10, pady=10)
+server_status.pack(in_=title_frame, padx=10, pady=10)
+
+# Port
+port_label = customtkinter.CTkLabel(app, text="Port")
+port_entry = customtkinter.CTkEntry(app, corner_radius=0)
+port_label.pack(in_=port_frame, side=tkinter.LEFT, padx=5, pady=10)
+port_entry.pack(in_=port_frame, side=tkinter.RIGHT, padx=5, pady=10)
+port_entry.insert(0, str(default_port))
+
+# Start / Stop
+server_start_button = customtkinter.CTkButton(app, text="Start Server", command=start_server)
+server_stop_button = customtkinter.CTkButton(app, text="Stop Server", command=stop_server)
+server_start_button.pack(in_=server_toggle_frame, side=tkinter.LEFT, padx=10, pady=10)
+server_stop_button.pack(in_=server_toggle_frame, side=tkinter.LEFT, padx=10, pady=10)
 
 
 # Run
