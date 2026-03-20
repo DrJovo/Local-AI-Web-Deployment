e6c058b218eabcd15549ec2af452f6962b081265
Dr. Jovo <jonathanckleven@gmail.com>
2026-03-19 21:59:44 -0700

    Improved the Text Field
    - Added a button to send the text.
    - Adjusted how the text field expands. Instead of expanding on focus, it expands when the text begins to overflow.

--- web/static/script.js
+++ web/static/script.js
@@ -1,16 +1,26 @@
-// -- Scale up prompt input field when clicked -- //
+// -- Scale up prompt input field when overflowing -- //
 var input_space = document.getElementById("input_space");
 var input_field = document.getElementById("input_field");
 var chat_space = document.getElementById("chat_space");
 
-// When in focus, grow big
-input_field.addEventListener("focus", function () {
-    chat_space.classList.add("typing");
-    input_field.classList.add("typing");
-});
+function CheckOverflow() {
+    const isOverflowing = input_field.scrollHeight > input_field.clientHeight;
+    if (isOverflowing && !input_field.classList.contains("typing")) {
+        chat_space.classList.add("typing");
+        input_field.classList.add("typing");
+    }
+    else if (!isOverflowing && input_field.innerHTML.length < 1) {
+        chat_space.classList.remove("typing");
+        input_field.classList.remove("typing");
+    }
+    window.requestAnimationFrame(CheckOverflow);
+}
 
-// When clicked off, shrink
-input_field.addEventListener("blur", function () {
-    chat_space.classList.remove("typing");
-    input_field.classList.remove("typing");
-});
+window.requestAnimationFrame(CheckOverflow);
+
+
+// -- Send message -- //
+function SendMessage() {
+    var text = input_field.textContent;
+    input_field.textContent = "";
+}
