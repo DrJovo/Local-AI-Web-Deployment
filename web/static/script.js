fc9d7b872a42a3995501f4dd44f161300c386d6d
Dr. Jovo <jonathanckleven@gmail.com>
2026-05-22 22:44:44 -0700

    Added Prompt Submission
    - You can now send prompts to the chat space, and they will show up as a text bubble.

--- web/static/script.js
+++ web/static/script.js
@@ -19,10 +19,49 @@ function CheckOverflow() {
 window.requestAnimationFrame(CheckOverflow);
 
 
+// -- Create user chat -- //
+function createPrompt(message) {
+    const div = document.createElement("div");
+
+    div.className = "user_chat";
+    div.innerHTML = `
+        <div>${message}</div>
+    `;
+
+    chat_space.appendChild(div);
+}
+
+
+// -- Create AI chat -- //
+function createAIResponse(message) {
+    const div = document.createElement("div");
+
+    div.className = "ai_chat";
+    div.innerHTML = `
+        <p>${message}</p>   
+
+        <button class="ai_copy" onclick="CopyText(this)">
+            <svg id="copy_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path fill="currentColor"
+                    d="M192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-200.6c0-17.4-7.1-34.1-19.7-46.2L370.6 17.8C358.7 6.4 342.8 0 326.3 0L192 0zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-16-64 0 0 16-192 0 0-256 16 0 0-64-16 0z"/>
+            </svg>
+        </button>
+    `;
+
+    chat_space.appendChild(div);
+}
+
+
 // -- Send message -- //
 function SendMessage() {
-    var text = input_field.textContent;
-    input_field.textContent = "";
+    var html = input_field.innerHTML;
+    
+    if (html.length > 0) {
+        input_field.innerHTML = "";
+
+        createPrompt(html);
+        createAIResponse("This is an example AI response.");
+    }
 }
 
 
